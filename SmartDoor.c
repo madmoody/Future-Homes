2213dd89c48f9cb035ab89b2726a16cfca0c8cd6
madmoody <73907801+madmoody@users.noreply.github.com>
2023-07-22 13:05:41 +0530

    Created

---
+++ SmartDoor.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define GPIO_PIN "/sys/class/gpio/gpio26/value"  // Replace "26" with the actual GPIO pin number
+
+// Function to unlock the door
+void unlock_door() {
+    int fd = open(GPIO_PIN, O_WRONLY);
+    if (fd < 0) {
+        perror("Error opening GPIO pin");
+        return;
+    }
+
+    // Turn ON the GPIO pin to unlock the door
+    write(fd, "1", 1);
+
+    // Add a delay (adjust as needed) to keep the door unlocked
+    usleep(2000000); // 2 seconds delay
+
+    // Turn OFF the GPIO pin to lock the door again
+    write(fd, "0", 1);
+
+    close(fd);
+}
+
+int main() {
+    // Export the GPIO pin
+    int export_fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (export_fd < 0) {
+        perror("Error exporting GPIO pin");
+        return 1;
+    }
+    write(export_fd, "26", 2);
+    close(export_fd);
+
+    // Set the GPIO pin direction as output
+    int direction_fd = open("/sys/class/gpio/gpio26/direction", O_WRONLY);
+    if (direction_fd < 0) {
+        perror("Error setting GPIO direction");
+        return 1;
+    }
+    write(direction_fd, "out", 3);
+    close(direction_fd);
+
+    // Unlock the door
+    unlock_door();
+
+    return 0;
+}
